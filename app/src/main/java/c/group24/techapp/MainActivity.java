d5d3aa3a4dd45c5dd68e3313934de0f52adc728e
Cancan Ai <can1@umd.edu>
2018-11-10 15:54:48 -0500

    new files added

--- app/src/main/java/c/group24/techapp/MainActivity.java
+++ app/src/main/java/c/group24/techapp/MainActivity.java
@@ -1,7 +1,10 @@
 package c.group24.techapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button test = findViewById(R.id.button5);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent test = new Intent(MainActivity.this,projectList.class);
+                startActivity(test);
+            }
+        });
     }
 }
